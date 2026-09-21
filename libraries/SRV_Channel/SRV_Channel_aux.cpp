4b87bc367b0dea5cc03ecac44e8e5fdbceafda9e
Huang Lukun <2636335620@qq.com>
2026-09-22 06:28:44 +0800

    SRV_Channel: configure k_tiltHexa1..6 tilt servos as angle outputs (set_angle 5000)

--- libraries/SRV_Channel/SRV_Channel_aux.cpp
+++ libraries/SRV_Channel/SRV_Channel_aux.cpp
@@ -190,6 +190,16 @@ void SRV_Channel::aux_servo_function_setup(void)
     case k_roll_out:
     case k_pitch_out:
     case k_yaw_out:
+        set_angle(5000);
+        break;
+    case k_tiltHexa1:
+    case k_tiltHexa2:
+    case k_tiltHexa3:
+    case k_tiltHexa4:
+    case k_tiltHexa5:
+    case k_tiltHexa6:
+        set_angle(5000);
+        break;
     case k_rcin1_mapped ... k_rcin16_mapped:
         set_angle(4500);
         break;
@@ -634,7 +644,7 @@ float SRV_Channels::get_output_scaled(SRV_Channel::Function function)
 float SRV_Channels::get_slew_limited_output_scaled(SRV_Channel::Function function)
 {
     if (!SRV_Channel::valid_function(function)) {
-        return 0.0;
+        return 0.0f;
     }
     for (slew_list *slew = _slew; slew; slew = slew->next) {
         if (slew->func == function) {

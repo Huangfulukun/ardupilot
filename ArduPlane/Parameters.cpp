ca40c554d0e33c527f6d98ea5b79fa78398feb37
Huang Lukun <2636335620@qq.com>
2026-09-22 06:37:44 +0800

    ArduPlane: byte-identical Parameters.cpp (THX_ subgroup index 41, +4 vs baseline)

--- ArduPlane/Parameters.cpp
+++ ArduPlane/Parameters.cpp
@@ -255,7 +255,7 @@ const AP_Param::Info Plane::var_info[] = {
 
     // @Param: WP_LOITER_RAD
     // @DisplayName: Waypoint Loiter Radius
-    // @Description: Defines the distance from the waypoint center, the plane will maintain during a loiter. If you set this value to a negative number then the default loiter direction will be counter-clockwise instead of clockwise. If the value is too close to zero, the achieved loiter radius will be determined by ROLL_LIMIT_DEG.
+    // @Description: Defines the distance from the waypoint center, the plane will maintain during a loiter. If you set this value to a negative number then the default loiter direction will be counter-clockwise instead of clockwise. If this value is too close to zero, the achieved loiter radius will be determined by ROLL_LIMIT_DEG.
     // @Units: m
     // @Range: -32767 32767
     // @Increment: 1
@@ -1561,7 +1561,7 @@ void Plane::load_parameters(void)
     {
         static const AP_Param::ConversionInfo gcs_conversion_info[] {
             { Parameters::k_param_sysid_this_mav_old, 0, AP_PARAM_INT16,  "MAV_SYSID" },
-            { Parameters::k_param_sysid_my_gcs_old, 0, AP_PARAM_INT16,  "MAV_GCS_SYSID" },
+            { Parameters::k_param_sysid_my_gcs_old, 0, AP_PARAM_INT16, "MAV_GCS_SYSID" },
             { Parameters::k_param_g2,  4, AP_PARAM_INT8, "MAV_OPTIONS" },
             { Parameters::k_param_telem_delay_old,  0, AP_PARAM_INT8, "MAV_TELEM_DELAY" },
         };
